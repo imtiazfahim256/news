695b17a2fe7c96eb609f96d781502fcf08509860
Imtiaz Fahim <61643273+imtiazfahim256@users.noreply.github.com>
2020-03-30 13:38:37 +0600

    online News

---
+++ assets/js/my.js
@@ -0,0 +1,14 @@
+ 
+    function openNav() {
+   
+    document.getElementById("mySidenav").style.width = "50%";
+    document.getElementById("close").style.transform="rotate(360deg)";
+
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+// scrollll function 
+
